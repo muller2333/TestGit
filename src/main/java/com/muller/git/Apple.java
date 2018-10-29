e88b7ee235ad079b92dad2ed86c81806c4602ed0
muller_glb <muller_glb@gmail.com>
2018-10-29 22:47:48 +0800

    测试

--- src/main/java/com/muller/git/Apple.java
+++ src/main/java/com/muller/git/Apple.java
@@ -4,7 +4,6 @@ public class Apple {
 	public static void main(String[] args) {
 		System.out.println("right");
 		System.out.println("hot fix...");
-		System.out.println("hot fix...");
 		System.out.println("xxxxxxxxxx");
 		System.out.println("yyyyyyyyyyyyyy");
 		System.out.println("zzzzzzzzzz");
