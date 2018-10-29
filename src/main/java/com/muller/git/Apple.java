de36f56bc12dedbcbfe559c82b8e4a933ff179d9
muller2333 <zhxzhx070@hotmail.com>
2018-10-29 22:29:00 +0800

    Update Apple.java

--- src/main/java/com/muller/git/Apple.java
+++ src/main/java/com/muller/git/Apple.java
@@ -4,6 +4,7 @@ public class Apple {
 	public static void main(String[] args) {
 		System.out.println("right");
 		System.out.println("hot fix...");
+		System.out.println("hot fix...");
 		System.out.println("xxxxxxxxxx");
 		System.out.println("yyyyyyyyyyyyyy");
 		System.out.println("zzzzzzzzzz");
